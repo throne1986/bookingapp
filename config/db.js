8939ac389d4149553149c97899b6f7b2f80c0e66
gruby-murzyn <jellyforjully088@yahoo.com>
2018-06-19 03:07:24 +0200

    Database Url Chages

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://makumba:makumba404@ds255260.mlab.com:55260/jasmine"
+    url: "mongodb://makumba:makumba404@ds163330.mlab.com:63330/jasmine"
+    //url: "mongodb://makumba:makumba404@ds255260.mlab.com:55260/jasmine"
 }
