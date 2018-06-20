f6c37c9d13588b18e139957a5f3b0a3671d8e548
gruby-murzyn <jellyforjully088@yahoo.com>
2018-06-20 09:01:10 +0200

    heroku deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const db = require('./app/config/database');
-
+const http = require('http');
 // Connect To Database
 mongoose.connect(db.database);
 
